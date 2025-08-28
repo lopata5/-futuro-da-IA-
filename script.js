2e8aa867c6663239be4f0600f9a4d25ca36d0e6d
lopata5 <145133302+lopata5@users.noreply.github.com>
2025-08-28 11:37:16 -0300

    ...

--- script.js
+++ script.js
@@ -3,3 +3,35 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultados");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas =  [
+    {
+        enunciado:
+             "Qual plataforma de IA você mais usa ?"
+        alternativas :[
+            "chat gpt ou chatgpt Brasil"
+        ],
+       },
+       {
+       enunciado:
+       "Você acha a IA importante ?"
+      alternativas:[
+        "sim ou não"
+      ],
+      },
+      {
+        enunciado:
+        "Você usa alguma IA para estudo ?"
+        alternativas:[
+            "sim ou não"
+        ],
+        },
+        {
+          enunciado:
+          "Oque é inteligência artificial ?"
+          alternativas:[
+            "Um tipo de rede social automatizada ou Um sistema que simula a inteligência humana e aprende com dados"
+          ],
+          }, 
+          
+
